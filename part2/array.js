c97819a624ac13eee87479806280e2ecb906beb1
DivyamThakkar <payalthakkar899@gmail.com>
2025-10-20 19:55:40 +0530

    changes

---
+++ part2/array.js
@@ -0,0 +1,8 @@
+const myArray = [0,1,2,3,4,5]
+const myHeros = ["ironman", "thor", "loki"]
+
+const myArr2 = new Array(0,2,3,4,5)
+console.log(myArray[0]); //shallow copy one same reference multiple copies.
+myArray.push(6)
+
+console.log(myArray)
