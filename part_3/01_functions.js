ea8670a07a0a376eb1fd5b14f478f7f289a2a37a
DivyamThakkar <payalthakkar899@gmail.com>
2025-12-14 22:47:09 +0530

    trying functions

---
+++ part_3/01_functions.js
@@ -0,0 +1,14 @@
+function Adder(a,b){
+    return a + b;
+}
+
+let a = Adder(10,5)
+console.log(a)
+
+function LoginMessage(username){
+    console.log(`${username} has just logged in`)
+}
+
+LoginMessage("Divyam")
+
+//upar parameter and andar argument
