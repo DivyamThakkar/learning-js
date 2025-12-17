470e5ec7d2441387a790b017a0f935f6c1c1f75f
DivyamThakkar <payalthakkar899@gmail.com>
2025-12-17 19:44:36 +0530

    functions complete scope videos initiated

--- part_3/01_functions.js
+++ part_3/01_functions.js
@@ -12,6 +12,36 @@ function LoginMessage(username){
     return (`${username} has just logged in`)
 }
 
-console.log(LoginMessage("Divyam"))
-console.log(LoginMessage())
+// console.log(LoginMessage("Divyam"))
+// console.log(LoginMessage())
 //upar parameter and andar argument
+
+
+// Multiple arguments and amount unknown
+
+
+//---------------------//
+//unkonwn number of multiple parameters
+
+
+// ... rest operator
+function ShoppingCart(...num1){
+    return num1;
+}
+console.log(ShoppingCart(200,300,500))
+
+const user = {
+    username: "divyam",
+    price: "100"
+
+}
+
+function handleObject(anyObject){
+    console.log(`The username is ${anyObject.username} and price is ${anyObject.price}`)
+
+}
+
+handleObject(user)
+
+//these functions dont work when instead of price u have a small change like prices
+//u can directly pass objects without declaring them outside 
---
+++ part_3/02_scope.js
@@ -0,0 +1,19 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+// console.table([a,b,c])
+
+//scope inside {} is called block scop
+
+if(true){
+let a = 10
+const b = 20
+var c = 30
+}
+// a's scope is inside {}
+// console.log(a)
+console.log(c)
+//var declared a golobal variable
+
+
